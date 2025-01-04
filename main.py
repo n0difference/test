d95a1951c522a239a202304bc53e6fce5ebea89e
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-05 01:32:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,52 +1,16 @@
-def max_number(a, b):
+def data_input():
     try:
-        if a > b:
-            return a
-        else:
-            return b
+        num = int(input("введите целое положительное число: "))
+        assert num > 0
+        return num
     except:
-        print("error max_number")
+        print("error: необходимо ввести целое положительное число")
 
 
-def empty_function():
-    pass
+def countdown(num):
+    while num >= 0:
+        print(num)
+        num -= 1
 
 
-def even_numbers(n):
-    try:
-        i = 0
-        while i <= n:
-            yield i
-            i += 2
-    except:
-        print("error even_numbers")
-
-
-def test_max_number(num1, num2):
-    try:
-        if max_number(num1, num2) == max(num1, num2):
-            print(f"ok: {max_number(num1, num2)}")
-        else:
-            print("error in test_max_number, incorrect answer: ", max_number(num1, num2)," | ", max(num1, num2))
-    except:
-        print("error in test_max_number")
-
-
-def data_input(n):
-    try:
-        print(f"введите {n} чисел")
-        numbers_array = [0] * n
-        for i in range(n):
-            numbers_array[i] = float(input())
-        return numbers_array
-    except ValueError:
-        print("error: необходимо ввести число")
-
-
-
-test_array = data_input(2)  # когда я пытелся передать напрямую, он дважды вызывал функцию
-                            # и я так и не понял как это сделать без промежуточного массива
-test_max_number(test_array[0], test_array[1])
-
-for i in even_numbers(data_input(1)[0]):
-    print(i)
+countdown(data_input())
