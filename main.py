a0e74264c52c829a3cd35107b6c6f1ccc58d4677
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-04 20:33:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ def data_input(str):
     return num1, num2
 
 
-def choice_of_action(str, num_tuple):
-    if str == "сложения":
-        return (num_tuple[0] + num_tuple[1])
-    elif str == "вычитания":
-        return (num_tuple[0] - num_tuple[1])
-    elif str == "умножения":
-        return (num_tuple[0] * num_tuple[1])
-    elif str == "деления":
-        try:
+def choice_of_action(act, num_tuple):
+    try:
+        if act == "сложения":
+            return (num_tuple[0] + num_tuple[1])
+        elif act == "вычитания":
+            return (num_tuple[0] - num_tuple[1])
+        elif act == "умножения":
+            return (num_tuple[0] * num_tuple[1])
+        elif act == "деления":
             return (num_tuple[0] / num_tuple[1])
-        except ZeroDivisionError:
-            print("Делитель не может быть равен нулю")
-    else:
-        return "error"
+        else:
+            return "error: неверное действие"
+    except ZeroDivisionError:
+        print("Делитель не может быть равен нулю")
 
 
-strAraay = ["сложения","вычитания","умножения","деления"]
+action_array = ["сложения","вычитания","умножения","деления"]
 
-for i in strAraay:
+for i in action_array:
     print(f"результат {i}:", choice_of_action(i, data_input(f"введите числа для {i}")))
