e5bd9060f3e6227f04fb7cd54344c0c1c762ed24
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-04 18:26:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-try:
-    num1 = float(input("введите 1 число:"))
-    num2 = float(input("введите 2 число:"))
-except ValueError:
-    print("необходимо ввести число")
-except ZeroDivisionError:
-    print("второе число не может быть 0")
-else:
-    print(f"{num1}/{num2}={num1 / num2}")
-finally:
-    print("конец программы")
+def vvod(str):
+    print(str)
+    num1 = float(input("введите 1 число: "))
+    num2 = float(input("введите 2 число: "))
+    return [num1, num2]
+
+def action(str, num1, num2):
+    if str == "сложения":
+        return (num1 + num2)
+    elif str == "вычитания":
+        return (num1 - num2)
+    elif str == "умножения":
+        return (num1 * num2)
+    elif str == "деления":
+        assert num2 != 0, print("Делитель не может быть равен нулю")
+        return (num1 / num2)
+    else:
+        return "error"
+
+
+strAraay = ["сложения","вычитания","умножения","деления"]
+resultArray =[]
+
+for i in strAraay:
+    resultArray = vvod(f"введите числа для {i}")
+    print(f"результат {i}:", action(i, resultArray[0], resultArray[1]))
