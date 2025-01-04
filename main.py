424b19fce01a62b7020a54e160d1df4d731b780e
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-04 22:36:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
-def data_input(title):
-    print(title)
-    num1 = float(input("введите 1 число: "))
-    num2 = float(input("введите 2 число: "))
-    return num1, num2
+def data_input():
+    age = int(input("введите возаст: "))
+    citizenship = int(input("введите \"1\", если вы являетесь гражданином и \"0\", если нет: "))
+    disqualification = int(input("введите \"1\", если вам запрещено участвовать в выборах и \"0\", если нет: "))
+    return age, citizenship, disqualification
 
 
-def choice_of_action(act, num_tuple):
-    try:
-        if act == "сложения":
-            return (num_tuple[0] + num_tuple[1])
-        elif act == "вычитания":
-            return (num_tuple[0] - num_tuple[1])
-        elif act == "умножения":
-            return (num_tuple[0] * num_tuple[1])
-        elif act == "деления":
-            return (num_tuple[0] / num_tuple[1])
-        else:
-            return "error: неверное действие"
-    except ZeroDivisionError:
-        print("Делитель не может быть равен нулю")
+def check_permition(info_tuple):
+    if (info_tuple[0] >= 18 and info_tuple[1] and not info_tuple[2]):
+        print("вы можете голосовать")
+    else:
+        print("вы не можете голосовать")
 
 
-action_array = ["сложения","вычитания","умножения","деления"]
-
-for i in action_array:
-    print(f"результат {i}:", choice_of_action(i, data_input(f"введите числа для {i}")))
+check_permition(data_input())
