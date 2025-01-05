cbda98ce3445042e803986ff60324f2dfd31507d
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-05 15:07:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 def max_number(a, b):
-    try:
-        if a > b:
-            return a
-        else:
-            return b
-    except:
-        print("error max_number")
+    if a > b:
+        return a
+    else:
+        return b
 
 
 def empty_function():
@@ -13,40 +10,34 @@ def empty_function():
 
 
 def even_numbers(n):
-    try:
-        i = 0
-        while i <= n:
-            yield i
-            i += 2
-    except:
-        print("error even_numbers")
+    i = 0
+    while i <= n:
+        yield i
+        i += 2
 
 
-def test_max_number(num1, num2):
-    try:
-        if max_number(num1, num2) == max(num1, num2):
-            print(f"ok: {max_number(num1, num2)}")
-        else:
-            print("error in test_max_number, incorrect answer: ", max_number(num1, num2)," | ", max(num1, num2))
-    except:
-        print("error in test_max_number")
+def test_max_number(num1, num2, answer):
+    if max_number(num1, num2) == answer:
+        print(f"ok: {max_number(num1, num2)}")
+    else:
+        print("error in test_max_number, incorrect answer: ", max_number(num1, num2)," | ", answer)
 
 
-def data_input(n):
-    try:
-        print(f"введите {n} чисел")
-        numbers_array = [0] * n
-        for i in range(n):
-            numbers_array[i] = float(input())
-        return numbers_array
-    except ValueError:
-        print("error: необходимо ввести число")
+try:
+    print("введите 2 чисела и большее среди них")
+    num1 = float(input())
+    num2 = float(input())
+    answer = float(input())
+except ValueError:
+    print("error: необходимо ввести число")
 
+test_max_number(num1, num2, answer)
 
+try:
+    print("введите чисело для отсчета")
+    num = float(input())
+except ValueError:
+    print("error: необходимо ввести число")
 
-test_array = data_input(2)  # когда я пытелся передать напрямую, он дважды вызывал функцию
-                            # и я так и не понял как это сделать без промежуточного массива
-test_max_number(test_array[0], test_array[1])
-
-for i in even_numbers(data_input(1)[0]):
+for i in even_numbers(num):
     print(i)
