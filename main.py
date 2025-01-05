e80a12038f9d575f850f66f0b431e4c94b130d50
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-06 02:19:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -16,22 +16,19 @@ def even_numbers(n):
         i += 2
 
 
-def test_max_number(num1, num2, answer):
-    if max_number(num1, num2) == answer:
-        print(f"ok: {max_number(num1, num2)}")
-    else:
-        print("error in test_max_number, incorrect answer: ", max_number(num1, num2)," | ", answer)
+def test_max_number():
+    test_list = [[5, 2, 5], [-1, 3, 3], [-5, -4, -4], [2.2, 3, 3], [4.4, -1, 4.4], [-5.2, -99.1, -5.2]]
+    for i in test_list:
+        assert  ((type(i[0]) == int or type(i[0]) == float) and \
+                (type(i[1]) == int or type(i[1]) == float) and \
+                (type(i[2]) == int or type(i[2]) == float)), "аргументы должны быть числами"
+        if max_number(i[0], i[1]) == i[2]:
+            print(f"ok: {max_number(i[0], i[1])}")
+        else:
+            print("error in test_max_number, incorrect answer: ", max_number(i[2], i[1])," | ", i[2])
 
 
-try:
-    print("введите 2 чисела и большее среди них")
-    num1 = float(input())
-    num2 = float(input())
-    answer = float(input())
-except ValueError:
-    print("error: необходимо ввести число")
-
-test_max_number(num1, num2, answer)
+test_max_number()
 
 try:
     print("введите чисело для отсчета")
