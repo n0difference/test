ec844a0f69d781a8e8e7f550ac6a9ddba7934dfe
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-07 21:00:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-def even_sum():
-    even_sum = 0
-    for i in range(0,101,2):
-        even_sum += i
-    return even_sum
-
-
-def num_counter():
-    num = 0
-    count = 0
-    while num >= 0:
-        num = float(input("введите число: "))
-        count += 1
-    return count
-
-
-print(even_sum())
-
-squares = [n**2 for n in range(1,11,2)]
-print(squares)
-
-print(num_counter())
+list = [1, 2, 3, 4, 5]
+list[0], list[4] = list[4], list[0]
+print(list)
