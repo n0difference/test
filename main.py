39050029d5193536ec015fd2401ae27574918837
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-07 20:36:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,22 @@
-def data_input():
-    text = input("введите текст: ").lower()
-    text.replace(".", "")
-    text.replace(",", "")
-    return text
+def even_sum():
+    even_sum = 0
+    for i in range(0,101,2):
+        even_sum += i
+    return even_sum
 
 
-def count_words(text):
-    return len(text.split())
+def num_counter():
+    num = 0
+    count = 0
+    while num >= 0:
+        num = float(input("введите число: "))
+        count += 1
+    return count
 
 
-def longest_word(text):
-    length = ""
-    for i in text.split():
-        if len(i) > len(length):
-            length = i
-    return length
+print(even_sum())
 
+squares = [n**2 for n in range(1,11,2)]
+print(squares)
 
-def count_vowels(text):
-    vowels = "аеёиоуыэюя"
-    counter = 0
-    for i in text:
-        if i in vowels:
-            counter += 1
-    return counter
-
-
-def every_word_count(text):
-    word_list = {}
-    for i in text.split():
-        if i in word_list:
-            word_list[i] += 1
-        else:
-            word_list[i] = 1
-    return word_list
-
-
-text = data_input()
-
-print(f"количество слов: {count_words(text)}")
-print(f"самое длинное слово: {longest_word(text)}")
-print(f"количество гласных букв: {count_vowels(text)}")
-print(f"количество раз, которое каждое слово встречается в тексте: {every_word_count(text)}")
+print(num_counter())
