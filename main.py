9c743109d5fcaf33ddadf36988325beafd71572f
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-07 19:14:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-real_password = "qwerty123"
-enterned_password = ""
+def data_input():
+    text = input("введите текст: ").lower()
+    text.replace(".", "")
+    text.replace(",", "")
+    return text
 
-while real_password != enterned_password:
-    enterned_password = input("введите пароль: ")
-    if enterned_password == real_password:
-        print("пароль верный")
-    else:
-        print("пароль не верный")
+
+def count_words(text):
+    return len(text.split())
+
+
+def longest_word(text):
+    length = ""
+    for i in text.split():
+        if len(i) > len(length):
+            length = i
+    return length
+
+
+def count_vowels(text):
+    vowels = "аеёиоуыэюя"
+    counter = 0
+    for i in text:
+        if i in vowels:
+            counter += 1
+    return counter
+
+
+def every_word_count(text):
+    word_list = {}
+    for i in text.split():
+        if i in word_list:
+            word_list[i] += 1
+        else:
+            word_list[i] = 1
+    return word_list
+
+
+text = data_input()
+
+print(f"количество слов: {count_words(text)}")
+print(f"самое длинное слово: {longest_word(text)}")
+print(f"количество гласных букв: {count_vowels(text)}")
+print(f"количество раз, которое каждое слово встречается в тексте: {every_word_count(text)}")
