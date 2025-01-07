c7a4cc9785118c24d1e18360203164d1e28aeb1d
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-07 03:32:00 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -19,16 +19,11 @@ def even_numbers(n):
 def test_max_number():
     test_list = [[5, 2, 5], [-1, 3, 3], [-5, -4, -4], [2.2, 3, 3], [4.4, -1, 4.4], [-5.2, -99.1, -5.2]]
     for i in test_list:
-        assert  ((type(i[0]) == int or type(i[0]) == float) and \
-                (type(i[1]) == int or type(i[1]) == float) and \
-                (type(i[2]) == int or type(i[2]) == float)), "аргументы должны быть числами"
-        if max_number(i[0], i[1]) == i[2]:
-            print(f"ok: {max_number(i[0], i[1])}")
-        else:
-            print("error in test_max_number, incorrect answer: ", max_number(i[2], i[1])," | ", i[2])
+        assert max_number(i[0], i[1]) == i[2], f"error: max_number({i[0]}, {i[1]}) = {max_number(i[0], i[1])}, must: {i[2]}"
+    return "ok"
 
 
-test_max_number()
+print(test_max_number())
 
 try:
     print("введите чисело для отсчета")
