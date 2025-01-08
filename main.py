5677a3c179e9840d889ffea8223bd0b2d50e9387
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-08 15:37:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-def max_number(a, b):
-    if a > b:
-        return a
-    else:
-        return b
-
-
-def empty_function():
-    pass
-
-
-def even_numbers(n):
-    i = 0
-    while i <= n:
-        yield i
-        i += 2
-
-
-def test_max_number():
-    test_list = [[5, 2, 5], [-1, 3, 3], [-5, -4, -4], [2.2, 3, 3], [4.4, -1, 4.4], [-5.2, -99.1, -5.2]]
-    for i in test_list:
-        assert max_number(i[0], i[1]) == i[2], f"error: max_number({i[0]}, {i[1]}) = {max_number(i[0], i[1])}, must: {i[2]}"
-    return "ok"
-
-
-print(test_max_number())
-
-try:
-    print("введите чисело для отсчета")
-    num = float(input())
-except ValueError:
-    print("error: необходимо ввести число")
-
-for i in even_numbers(num):
-    print(i)
+test_list = [1, 1, 2, 3]
+test_set = set(test_list)
+print(len(test_set))
