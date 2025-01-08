43014bbf0e41fbe77663db968468938ba0b0b0f2
n0difference <134058450+n0difference@users.noreply.github.com>
2025-01-08 18:25:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 def calculate_average(grades):
-    average = 0
-    for i in grades:
-        average += i
-    return (average/len(grades))
+    if grades:
+        average = 0
+        for i in grades:
+            average += i
+        return (average/len(grades))
+    else:
+        print("нет оценок")
+        return 0
 
 
 def student_info(students):
-    for i in students:
-        print(f"Студент: {i['name']}",
-              f"Средний балл: {calculate_average(i['grades'])}",
-              f"Статус: {'Отстающий' if calculate_average(i['grades']) < 75 else 'Успешен'}", sep="\n")
+    for student in students:
+        average = calculate_average(student['grades'])
+        student_status = 'Отстающий' if average < 75 else 'Успешен'
+        print(f"Студент: {student['name']}",
+              f"Средний балл: {average}",
+              f"Статус: {student_status}", sep="\n")
 
 
 def worst_student(students):
-    worst = {"name": "", "grades": [1000]}
-    for i in students:
-        if calculate_average(i["grades"]) < calculate_average(worst["grades"]):
-            worst = i
-    return worst
+    if students:
+        worst = {"name": "", "grades": [1000]}
+        for i in students:
+            if calculate_average(i["grades"]) < calculate_average(worst["grades"]):
+                worst = i
+        return worst
+    else:
+        print("нет студентов")
+
+
+def average_of_averages(students):
+    if students:
+        counter = 0
+        for student in students:
+            counter += calculate_average(student['grades'])
+        return (counter/len(students))
+    else:
+        print("нет студентов")
 
 
 students = [{"name": "Alice", "grades": [90, 95, 100]},
@@ -33,7 +52,11 @@ students.append({"name": "Rick", "grades": [100, 100, 100]})
 
 student_info(students)
 
-students.remove(worst_student(students))
+if worst_student(students):
+    students.remove(worst_student(students))
+else:
+    print("нет студентов для отчисления")
 
 student_info(students)
 
+print(f"Средний балл по всем студентам: {average_of_averages(students)}")
